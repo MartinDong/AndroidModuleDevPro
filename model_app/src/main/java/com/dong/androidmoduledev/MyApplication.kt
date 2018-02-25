3d87b33e037d4cc617aef9191b1f0d2ab97eb1b1
donghongyu <1358506549@qq.com>
2018-02-25 13:40:54 +0800

    加入了【dex多分包配置信息】

---
+++ model_app/src/main/java/com/dong/androidmoduledev/MyApplication.kt
@@ -0,0 +1,10 @@
+package com.dong.androidmoduledev
+
+import android.support.multidex.MultiDexApplication
+
+/**
+ * 管理自己Application
+ * Created by Kotlin on 2018/2/25.
+ */
+class MyApplication : MultiDexApplication() {
+}
