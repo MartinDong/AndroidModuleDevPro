134b7b653ea1b5c13493d13aaf46e243d9564de2
donghongyu <1358506549@qq.com>
2018-02-25 23:21:28 +0800

    修改掉了拼写错误的model名称。

---
+++ module_app/src/main/java/com/dong/module_app/MyApplication.kt
@@ -0,0 +1,21 @@
+package com.dong.module_app
+
+import android.content.Context
+import android.support.multidex.MultiDex
+import com.dong.lib_common.base.BaseApplication
+
+/**
+ * <p>这里仅需做一些初始化的工作</p>
+ *
+ * Created by Kotlin on 2018/2/25.
+ */
+class MyApplication : BaseApplication() {
+
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        // dex突破65535的限制
+        MultiDex.install(this)
+    }
+
+}
---
+++ module_news/src/main/java/com/dong/module_news/NewsActivity.kt
@@ -0,0 +1,12 @@
+package com.dong.module_news
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
