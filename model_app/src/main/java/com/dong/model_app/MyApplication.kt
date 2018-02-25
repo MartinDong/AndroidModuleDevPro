34c90017333fe5cc11a8e75ec7b93622a99875d6
donghongyu <1358506549@qq.com>
2018-02-25 22:42:47 +0800

    修復了因爲修改包名格式导致不对称的bug； 引入了ARouter

--- model_app/src/main/java/com/dong/model_app/MyApplication.kt
+++ model_app/src/main/java/com/dong/model_app/MyApplication.kt
@@ -1,10 +1,21 @@
 package com.dong.model_app
 
-import android.support.multidex.MultiDexApplication
+import android.content.Context
+import android.support.multidex.MultiDex
+import com.dong.lib_common.base.BaseApplication
 
 /**
- * 管理自己Application
+ * <p>这里仅需做一些初始化的工作</p>
+ *
  * Created by Kotlin on 2018/2/25.
  */
-class MyApplication : MultiDexApplication() {
+class MyApplication : BaseApplication() {
+
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        // dex突破65535的限制
+        MultiDex.install(this)
+    }
+
 }
