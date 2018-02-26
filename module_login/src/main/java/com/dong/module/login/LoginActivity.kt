64d1509c3baa82ae06fd9e2059eea8b7b460c404
donghongyu <donghongyu@jsj.com.cn>
2018-02-26 12:21:30 +0800

    修改了组件中的资源命名方式，防止合并的时候出现丢失问题。

--- module_login/src/main/java/com/dong/module/login/LoginActivity.kt
+++ module_login/src/main/java/com/dong/module/login/LoginActivity.kt
@@ -3,11 +3,11 @@ package com.dong.module.login
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
--- module_news/src/main/java/com/dong/module/news/NewsActivity.kt
+++ module_news/src/main/java/com/dong/module/news/NewsActivity.kt
@@ -7,6 +7,6 @@ class NewsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
     }
 }
