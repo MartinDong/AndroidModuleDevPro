fc0fd26fc109c89d74febc9522d6118fbdfb096e
donghongyu <donghongyu@jsj.com.cn>
2018-03-02 14:33:55 +0800

    重新封装了sqlite存储的文件地址设置，拒绝写死，默认的存储路径为data/data/<packageName>/app_database/myApp.db

--- lib_common/src/main/java/com/dong/lib/common/sqlite/BaseDaoFactory.kt
+++ lib_common/src/main/java/com/dong/lib/common/sqlite/BaseDaoFactory.kt
@@ -1,12 +1,24 @@
 package com.dong.lib.common.sqlite
 
+import android.content.Context
 import android.database.sqlite.SQLiteDatabase
+import com.dong.lib.common.utils.Utils
 
 /**
  * <p>数据库处理工厂</p>
  * Created by Kotlin on 2018/3/1.
  */
 class BaseDaoFactory {
+
+    //默认数据库名称
+    private var dbName = "myApp.db"
+
+    init {
+        //openOrCreateDatabase 如果不存在则先创建再打开数据库，如果存在则直接打开。
+        sqLiteDatabasePath = "${Utils.getContext().getDir("database", Context.MODE_APPEND).path}/$dbName"
+        sqLiteDatabase = SQLiteDatabase.openOrCreateDatabase(sqLiteDatabasePath, null)
+    }
+
     companion object {
         //单利工厂
         private var baseDaoFactory: BaseDaoFactory? = null
@@ -27,12 +39,6 @@ class BaseDaoFactory {
         }
     }
 
-    constructor() {
-        sqLiteDatabasePath = "data/data/com.dong.module.app.debug/donghongyu.db"
-        //openOrCreateDatabase 如果不存在则先创建再打开数据库，如果存在则直接打开。
-        sqLiteDatabase = SQLiteDatabase.openOrCreateDatabase(sqLiteDatabasePath, null)
-    }
-
     //获取数据库操作对象
     fun <T> getBaseDao(entityClass: Class<T>): BaseDao<T>? {
         var baseDao: BaseDao<T>? = null
@@ -42,4 +48,5 @@ class BaseDaoFactory {
 
         return baseDao
     }
+
 }
