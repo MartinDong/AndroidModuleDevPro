03f0dcc59ac020817b01173b983c895f8fef993d
donghongyu <donghongyu@jsj.com.cn>
2018-03-02 17:28:30 +0800

    加入了传统方式进行数据表创建

--- module_sqlite/src/main/java/com/dong/module/sqlite/initdb/OldWayInitActivity.kt
+++ module_sqlite/src/main/java/com/dong/module/sqlite/initdb/OldWayInitActivity.kt
@@ -1,9 +1,12 @@
 package com.dong.module.sqlite.initdb
 
+import android.database.SQLException
+import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.os.Bundle
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.dong.lib.common.base.BaseActivity
+import com.dong.lib.common.utils.ToastUtils
 import com.dong.module.sqlite.R
 import kotlinx.android.synthetic.main.initdb_old_way_init_activity.*
 
@@ -17,6 +20,17 @@ class OldWayInitActivity : BaseActivity() {
 
     //自己实现SQLiteOpenHelper
     private var customerSQLiteOpenHelper: SQLiteOpenHelper? = null
+    //数据库操作实例
+    private var sqLiteDatabase: SQLiteDatabase? = null
+    //记录数据库是否连接
+    private var isInit = false
+
+    //数据库里的表SQL
+    private var CREAT_TABLE_SQL = ("create table if not exists bookStore ("
+            + "id integer primary key autoincrement,"
+            + "book_name text, "
+            + "author text, "
+            + "price real)")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +41,28 @@ class OldWayInitActivity : BaseActivity() {
 
         btn_init_db.setOnClickListener {
             //通过getWritableDatabase()方式来新建SQLite数据库（2）
-            customerSQLiteOpenHelper!!.writableDatabase
+            try {
+                sqLiteDatabase = customerSQLiteOpenHelper!!.writableDatabase
+                if (sqLiteDatabase != null) {
+                    isInit = true
+                    ToastUtils.showShortToast(getText(R.string.module_sqlite_toast_init_db_success))
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        btn_init_db_table.setOnClickListener {
+            if (isInit) {
+                try {
+                    //执行创建表SQL语句
+                    sqLiteDatabase!!.execSQL(CREAT_TABLE_SQL)
+                } catch (e: SQLException) {
+                    e.printStackTrace()
+                }
+            } else {
+                ToastUtils.showShortToast(getText(R.string.module_sqlite_toast_init_db_tab_success))
+            }
         }
     }
 }
