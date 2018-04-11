c8490511899617d24fa99d7d9d6105de7268124a
MartinDong <1358506549@qq.com>
2018-04-11 17:38:39 +0800

    总结了文章 https://www.jianshu.com/p/a37fb8b30f13

--- module_knowledge/src/main/java/com/dong/module/knowledge/handler/CustomerHandlerActivity.java
+++ module_knowledge/src/main/java/com/dong/module/knowledge/handler/CustomerHandlerActivity.java
@@ -4,9 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.dong.module.knowledge.R;
@@ -14,83 +11,87 @@ import com.dong.module.knowledge.handler.customHandler.Handler;
 import com.dong.module.knowledge.handler.customHandler.Looper;
 import com.dong.module.knowledge.handler.customHandler.Message;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.lang.ref.WeakReference;
 
 /**
  * ================================================
- * 类说明
+ * 测试自定义Handler机制
  * Created by KotlinD on 2018/4/9.
  * <p>
  * ================================================
  */
 @Route(path = "/knowledge/customer_handler")
 public class CustomerHandlerActivity extends AppCompatActivity {
-    public Button btnStartCountDown;
-
-    public TextView tvCountDown;
-
     private static final int UPDATE = 0x1;
 
+    //默认是在主线程中执行
+    private MyHandler mHandler = null;
+
+    //继承实现自己的Handler，处理子线程与主线程的通讯交互
+    static class MyHandler extends Handler {
+        //使用弱引用，防止内存泄露
+        private final WeakReference<CustomerHandlerActivity> mActivity;
+
+        private MyHandler(CustomerHandlerActivity mActivity) {
+            this.mActivity = new WeakReference<>(mActivity);
+        }
+
+        //重写handleMessage进行处理接收到的Message
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            //获取引用的UI主线程Activity，用来获取UI线程的控件等
+            CustomerHandlerActivity activity = mActivity.get();
+            if (activity != null) {
+                //分发处理消息
+                switch (msg.what) {
+                    case UPDATE:
+                        //打印Log
+                        Log.i("TAG", "还有 " + String.valueOf(msg.arg1) + " 秒");
+                        break;
+                }
+            }
+        }
+    }
+
+    //模拟子线程进行耗时任务
+    private final Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            //这里做的是一个倒计时定时一秒发送一次数据
+            for (int i = 60; i > 0; i--) {
+                //构建属于子线程的Message
+                Message msg = new Message();
+                msg.what = UPDATE;
+                msg.arg1 = i;
+
+                //通过主线程中的Handler实例进行消息发送，将子线程中的消息发送到主线程中
+                mHandler.sendMessage(msg);
+                try {
+                    //休眠1秒
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_handler);
 
-        setContentView(R.layout.activity_customer_handler);
+        //在当前的线程中准备一个消息轮询器
+        Looper.prepare();
 
-        btnStartCountDown = findViewById(R.id.btn_start_count_down);
-        tvCountDown = findViewById(R.id.tv_count_down);
+        //创建主线程的Handler
+        mHandler = new MyHandler(this);
 
-        btnStartCountDown.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        //开启子线程运行耗时操作
+        new Thread(mRunnable).start();
 
-                //模拟系统的线程Looper的开启
-                Looper.prepare();
-
-                ExecutorService exe = Executors.newCachedThreadPool();
-
-                final Handler mHandler = new Handler() {
-                    @Override
-                    public void handleMessage(Message msg) {
-                        super.handleMessage(msg);
-                        switch (msg.what) {
-                            case UPDATE:
-                                tvCountDown.setText("还有" + String.valueOf(msg.obj) + "秒");
-
-                                //打印Log
-                                Log.i("TAG", "当前接收到线程 " + msg.obj + " 的消息");
-                                break;
-                        }
-                    }
-                };
-
-                for (int i = 3; i > 0; i--) {
-                    exe.execute(new Runnable() {
-                        @Override
-                        public void run() {
-                            while (true) {
-                                Message msg = new Message();
-                                msg.what = UPDATE;
-                                msg.obj = Thread.currentThread().getId();
-                                mHandler.sendMessage(msg);
-
-                                try {
-                                    //休眠1秒
-                                    Thread.sleep(1000);
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }
-                    });
-                }
-
-                Looper.loop();
-            }
-        });
-
+        //这里开启消息轮询器会造成UI的绘制阻塞属于正常现象
+        Looper.loop();
     }
-
 }
